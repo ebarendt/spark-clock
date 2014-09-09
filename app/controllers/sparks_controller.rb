7cbcde230c270d49820b9cf5fa823638833a06de
Eric Barendt <ebarendt@gmail.com>
2014-09-08 20:46:29 -0700

    better device creation; add flash messages to layout

--- app/controllers/sparks_controller.rb
+++ app/controllers/sparks_controller.rb
@@ -2,16 +2,14 @@ class SparksController < ApplicationController
   before_action :setup_device
 
   def show
-    spark = Spark.new(@device)
-    response = spark.query("temperature")
+    response = @spark.query("temperature")
     @temperature = sprintf("%.2f", response["result"])
     @core_info = response["coreInfo"]
   end
 
   def update
-    spark = Spark.new(@device)
-    if spark.command(params[:status])
-      redirect_to sparks_path
+    if @spark.command(params[:status])
+      redirect_to sparks_path, notice: "Changed to #{params[:status]}"
     else
       flash.now[:error] = spark.message
       render 'show'
@@ -21,6 +19,7 @@ class SparksController < ApplicationController
   private
 
   def setup_device
-    @device = ENV.fetch("CLOCK")
+    device = ENV.fetch("CLOCK")
+    @spark = Spark.new(device)
   end
 end
